09a42b3e0950bb08303d584e62ee0b56bdd1489e
acosi <cosianthony@gmail.com>
2026-03-14 02:44:01 +0100

    Fixed C comments to match subject

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-/* outside comment */
+/*
+    outside comment
+*/
 int output(void)
 {
-    const char *s = "#include <stdio.h>%c%c/* outside comment */%cint output(void)%c{%c    const char *s = %c%s%c;%c    if (printf(s,10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10) < 0)%c        return 1;%c    return ferror(stdout);%c}%c%cint main(void)%c{%c    // inside comment%c    return output();%c}%c";
-    if (printf(s,10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10) < 0)
+    const char *s = "#include <stdio.h>%c%c/*%c    outside comment%c*/%cint output(void)%c{%c    const char *s = %c%s%c;%c    if (printf(s,10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10) < 0)%c        return 1;%c    return ferror(stdout);%c}%c%cint main(void)%c{%c/*%c    inside comment%c*/%c    return output();%c}%c";
+    if (printf(s,10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10) < 0)
         return 1;
     return ferror(stdout);
 }
 
 int main(void)
 {
-    // inside comment
+/*
+    inside comment
+*/
     return output();
 }
