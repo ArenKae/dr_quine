7f64038d0051a3c11e5437c2fdc1167a590dcec1
acosi <cosianthony@gmail.com>
2026-03-14 01:16:07 +0100

    Refactor Colleen.s to streamline main function and improve readability

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -3,12 +3,12 @@ global main
 extern printf
 
 section .data
-    fmt db 'default rel',10,'global main',10,'extern printf',10,10,'section .data',10,'    fmt db %s,0',10,'    src db %c%s%c,0',10,10,'section .text',10,'; outside comment',10,'print:',10,'    ; inside comment',10,'    lea rdi, [fmt]',10,'    lea rsi, [src]',10,'    mov edx, 34',10,'    lea rcx, [src]',10,'    mov r8d, 34',10,'    xor eax, eax',10,'    call printf wrt ..plt',10,'    ret',10,10,'main:',10,'    call print',10,'    xor eax, eax',10,'    ret',10,10,'section .note.GNU-stack noalloc noexec nowrite progbits',10,0
-    src db "'default rel',10,'global main',10,'extern printf',10,10,'section .data',10,'    fmt db %s,0',10,'    src db %c%s%c,0',10,10,'section .text',10,'; outside comment',10,'print:',10,'    ; inside comment',10,'    lea rdi, [fmt]',10,'    lea rsi, [src]',10,'    mov edx, 34',10,'    lea rcx, [src]',10,'    mov r8d, 34',10,'    xor eax, eax',10,'    call printf wrt ..plt',10,'    ret',10,10,'main:',10,'    call print',10,'    xor eax, eax',10,'    ret',10,10,'section .note.GNU-stack noalloc noexec nowrite progbits',10",0
+    fmt db 'default rel',10,'global main',10,'extern printf',10,10,'section .data',10,'    fmt db %s,0',10,'    src db %c%s%c,0',10,10,'section .text',10,'; outside comment',10,'main:',10,'    ; inside comment',10,'    lea rdi, [fmt]',10,'    lea rsi, [src]',10,'    mov edx, 34',10,'    lea rcx, [src]',10,'    mov r8d, 34',10,'    xor eax, eax',10,'    call printf wrt ..plt',10,'    xor eax, eax',10,'    ret',10,10,'section .note.GNU-stack noalloc noexec nowrite progbits',10,0
+    src db "'default rel',10,'global main',10,'extern printf',10,10,'section .data',10,'    fmt db %s,0',10,'    src db %c%s%c,0',10,10,'section .text',10,'; outside comment',10,'main:',10,'    ; inside comment',10,'    lea rdi, [fmt]',10,'    lea rsi, [src]',10,'    mov edx, 34',10,'    lea rcx, [src]',10,'    mov r8d, 34',10,'    xor eax, eax',10,'    call printf wrt ..plt',10,'    xor eax, eax',10,'    ret',10,10,'section .note.GNU-stack noalloc noexec nowrite progbits',10",0
 
 section .text
 ; outside comment
-print:
+main:
     ; inside comment
     lea rdi, [fmt]
     lea rsi, [src]
@@ -17,10 +17,6 @@ print:
     mov r8d, 34
     xor eax, eax
     call printf wrt ..plt
-    ret
-
-main:
-    call print
     xor eax, eax
     ret
 
